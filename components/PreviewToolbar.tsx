dd644807478266f728c7a31d55b7e57e82fee8ca
Reid Barber <reid@reidbarber.com>
2023-12-06 16:26:14 -0600

    add shadow to toolbar

--- components/PreviewToolbar.tsx
+++ components/PreviewToolbar.tsx
@@ -1,6 +1,6 @@
 export default function PreviewToolbar({ children }) {
   return (
-    <div className="absolute top-0 right-0 w-[51%] z-[4] bg-[var(--sp-colors-surface1)] border-none h-500 py-50 px-200">
+    <div className="absolute top-0 right-0 w-full z-[4] shadow-sm bg-[var(--sp-colors-surface1)] border-none h-500 py-50 px-200">
       {children}
     </div>
   );
