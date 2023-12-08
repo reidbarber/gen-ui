c46f5b508471f2deac4399a31cce3ea364d99399
Reid Barber <reid@reidbarber.com>
2023-12-08 10:33:34 -0600

    move assistant dialog to its own components

---
+++ components/AssistantDialog.tsx
@@ -0,0 +1,87 @@
+import {
+  Button,
+  ButtonGroup,
+  Content,
+  Dialog,
+  Divider,
+  Heading,
+  Link,
+  ProgressCircle,
+} from "@adobe/react-spectrum";
+import { Radio, RadioGroup } from "react-aria-components";
+
+export function AssistantDialog({
+  assistants,
+  assistantId,
+  setSelectedAssistantId,
+  setShowAssistantDialog,
+}) {
+  return (
+    <Dialog>
+      <Heading>Select an Assistant</Heading>
+      <Divider />
+      <Content>
+        {assistants === null && (
+          <ProgressCircle aria-label="Loading assistants" />
+        )}
+        {assistants?.length === 0 && "No assistants found."}
+        {assistants?.length > 0 && (
+          <RadioGroup
+            className="flex items-center justify-center space-y-2 text-center"
+            aria-label="Available assistants"
+            value={assistantId}
+            onChange={setSelectedAssistantId}
+          >
+            {assistants?.map((assistant) => (
+              <Radio
+                key={assistant.id}
+                value={assistant.id}
+                className="flex justify-center bg-white border rounded dark:bg-black p-160 m-160 h-1200 w-2000 focus:outline-none focus-visible:ring-half focus-visible:ring-offset-0 selected:bg-accent-100 selected:border-accent-700"
+              >
+                {({ isSelected }) => (
+                  <div className="relative flex flex-col items-center justify-center w-full h-full gap-150">
+                    {isSelected && (
+                      <div className="absolute top-0 left-0 -mt-75 -ml-75">
+                        <div className="h-[14px] w-[14px] bg-accent-900 rounded-small">
+                          <svg
+                            className="fill-gray-75 pt-[2px] pl-[2px] w-[14px] h-[14px]"
+                            focusable="false"
+                            aria-hidden="true"
+                            role="img"
+                          >
+                            <path d="M3.788 9A.999.999 0 0 1 3 8.615l-2.288-3a1 1 0 1 1 1.576-1.23l1.5 1.991 3.924-4.991a1 1 0 1 1 1.576 1.23l-4.712 6A.999.999 0 0 1 3.788 9z"></path>
+                          </svg>
+                        </div>
+                      </div>
+                    )}
+                    <div className="font-semibold">{assistant.name}</div>
+                    {assistant.description && (
+                      <div className="text-sm">{assistant.description}</div>
+                    )}
+                  </div>
+                )}
+              </Radio>
+            ))}
+          </RadioGroup>
+        )}
+        {assistants !== null && (
+          <div className="text-center">
+            {assistants?.length > 0 && <div>or</div>}
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://platform.openai.com/assistants"
+            >
+              Create an assistant
+            </Link>
+          </div>
+        )}
+      </Content>
+      <ButtonGroup>
+        <Button variant="accent" onPress={() => setShowAssistantDialog(false)}>
+          Start
+        </Button>
+      </ButtonGroup>
+    </Dialog>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -28,6 +28,7 @@ import PreviewToolbar from "../components/PreviewToolbar";
 import { Assistant } from "openai/resources/beta/assistants/assistants";
 import { listAssistants } from "../api/assistants";
 import { Radio, RadioGroup } from "react-aria-components";
+import { AssistantDialog } from "../components/AssistantDialog";
 
 export default function Home(): JSX.Element {
   let [colorScheme, setColorScheme] = useState<"light" | "dark">("dark");
@@ -39,7 +40,7 @@ export default function Home(): JSX.Element {
     null
   );
   let assistantId = selectedAssistantId?.toString();
-  let [showSelectAssistant, setShowSelectAssistant] = useState(true);
+  let [showAssistantDialog, setShowAssistantDialog] = useState(true);
 
   // Load assistants
   useEffect(() => {
@@ -167,80 +168,6 @@ export default function Home(): JSX.Element {
     }
   };
 
-  function AssistantDialog() {
-    return (
-      <Dialog>
-        <Heading>Select an Assistant</Heading>
-        <Divider />
-        <Content>
-          {assistants === null && (
-            <ProgressCircle aria-label="Loading assistants" />
-          )}
-          {assistants?.length === 0 && "No assistants found."}
-          {assistants?.length > 0 && (
-            <RadioGroup
-              className="flex items-center justify-center space-y-2 text-center"
-              aria-label="Available assistants"
-              value={assistantId}
-              onChange={setSelectedAssistantId}
-            >
-              {assistants?.map((assistant) => (
-                <Radio
-                  key={assistant.id}
-                  value={assistant.id}
-                  className="flex justify-center bg-white border rounded dark:bg-black p-160 m-160 h-1200 w-2000 focus:outline-none focus-visible:ring-half focus-visible:ring-offset-0 selected:bg-accent-100 selected:border-accent-700"
-                >
-                  {({ isSelected }) => (
-                    <div className="relative flex flex-col items-center justify-center w-full h-full gap-150">
-                      {isSelected && (
-                        <div className="absolute top-0 left-0 -mt-75 -ml-75">
-                          <div className="h-[14px] w-[14px] bg-accent-900 rounded-small">
-                            <svg
-                              className="fill-gray-75 pt-[2px] pl-[2px] w-[14px] h-[14px]"
-                              focusable="false"
-                              aria-hidden="true"
-                              role="img"
-                            >
-                              <path d="M3.788 9A.999.999 0 0 1 3 8.615l-2.288-3a1 1 0 1 1 1.576-1.23l1.5 1.991 3.924-4.991a1 1 0 1 1 1.576 1.23l-4.712 6A.999.999 0 0 1 3.788 9z"></path>
-                            </svg>
-                          </div>
-                        </div>
-                      )}
-                      <div className="font-semibold">{assistant.name}</div>
-                      {assistant.description && (
-                        <div className="text-sm">{assistant.description}</div>
-                      )}
-                    </div>
-                  )}
-                </Radio>
-              ))}
-            </RadioGroup>
-          )}
-          {assistants !== null && (
-            <div className="text-center">
-              {assistants?.length > 0 && <div>or</div>}
-              <Link
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://platform.openai.com/assistants"
-              >
-                Create an assistant
-              </Link>
-            </div>
-          )}
-        </Content>
-        <ButtonGroup>
-          <Button
-            variant="accent"
-            onPress={() => setShowSelectAssistant(false)}
-          >
-            Start
-          </Button>
-        </ButtonGroup>
-      </Dialog>
-    );
-  }
-
   return (
     <Provider colorScheme={colorScheme} theme={defaultTheme} locale="en-US">
       <Head>
@@ -265,9 +192,16 @@ export default function Home(): JSX.Element {
       <PromptBar isGenerating={isGenerating} onSubmit={onSubmitPrompt} />
       <DialogContainer
         isDismissable={false}
-        onDismiss={() => setShowSelectAssistant(false)}
+        onDismiss={() => setShowAssistantDialog(false)}
       >
-        {showSelectAssistant && <AssistantDialog />}
+        {showAssistantDialog && (
+          <AssistantDialog
+            assistants={assistants}
+            assistantId={assistantId}
+            setSelectedAssistantId={setSelectedAssistantId}
+            setShowAssistantDialog={setShowAssistantDialog}
+          />
+        )}
       </DialogContainer>
     </Provider>
   );
