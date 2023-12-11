b53cd6c86f4fd34ae9ef7064df47838de85573fa
Reid Barber <reid@reidbarber.com>
2023-12-10 23:10:30 -0600

    use RSP Tabs for files

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -1,5 +1,4 @@
 import {
-  FileTabs,
   SandpackStack,
   useActiveCode,
   useSandpack,
@@ -7,6 +6,7 @@ import {
 import React from "react";
 import MonacoEditor from "@monaco-editor/react";
 import { getLanguageOfFile } from "../utils/utils";
+import { FileTabs } from "./FileTabs";
 
 export function Editor({ colorScheme }) {
   const { code, updateCode } = useActiveCode();
@@ -16,7 +16,8 @@ export function Editor({ colorScheme }) {
 
   return (
     <SandpackStack style={{ height: "100vh", margin: 0 }}>
-      <FileTabs className="z-[5]" style={{ border: "none" }} />
+      {/* <FileTabs className="z-[5]" style={{ border: "none" }} /> */}
+      <FileTabs />
       <div className="flex-1 bg-gray-50">
         <MonacoEditor
           options={{
